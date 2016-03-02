72c761cb7980646cb408551457da733def081bcb
JohnAlbertUW <JohnAlbertUW@users.noreply.github.com>
2016-03-02 10:22:41 -0500

    Update module2_lesson1_formative.rb

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,40 @@
+
+
+# Question 1.
+
 some_var = "false"
 another_var = "nil"
 
-if some_var == "pink elephant"
-  puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
-  puts "Question mark in the method name?"
-
-elsif some_var == false
-  puts "Looks like this one should execute"
-
-else
-  puts "I guess nothing matched... But why?"
+case
+	when some_var == "pink elephant"
+		puts "Don't think about the pink elephant!"
+		#some_var is false
+	when another_var.nil? # <= extremely confusing
+		puts "Question mark in the name?"
+		#nil is placed a string not a boolean
+	when some_var == false
+		puts "Looks like this one should execute?"
+		#not set as a boolean but rather a string
+	else 
+		puts "I guessed nothing matched but why?"
 end
+
+#Question 2 
+some_var = false
+another_var = nil
+
+#CASES TERMINATE AT FIRST TRUE CONDITION
+case
+	when some_var != "pink elephant"
+		puts "Don't think about the pink elephant!"
+		#some_var is false
+	when another_var == nil  # <= extremely confusing
+		puts "Question mark in the name?"
+		#nil is placed a string not a boolean
+	when some_var == false
+		puts "Looks like this one should execute?"
+		#not set as a boolean but rather a string
+	else 
+		puts "I guessed nothing matched but why?"
+end
+
